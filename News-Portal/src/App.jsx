5219fb55fe0b26bc40fde77ada0615bad02fff93
Abu-0-RayhaN <rayhan844625@gmail.com>
2023-11-19 01:09:21 +0600

    init

---
+++ News-Portal/src/App.jsx
@@ -0,0 +1,11 @@
+// import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-center bg-gray-300 font-bold text-3xl text-red-800">
+      App
+    </div>
+  );
+};
+
+export default App;
